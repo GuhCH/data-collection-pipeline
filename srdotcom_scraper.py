4856b00c19e417c89bb67c3f053bae6e2165a679
Joe Gocher <joegocher@gmail.com>
2022-03-09 14:28:51 +0000

    removes unused packages

--- srdotcom_scraper.py
+++ srdotcom_scraper.py
@@ -1,11 +1,8 @@
-from typing import List
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-import pandas as pd 
-import json
 import time
 
 class scraper:

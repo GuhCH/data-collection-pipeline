b3630d6e07e8e56be1820df623e163e9ce467936
Joe Gocher <joegocher@gmail.com>
2022-03-28 17:53:19 +0100

    - adds docstrings for all functions

--- srdotcom_scraper.py
+++ srdotcom_scraper.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import boolean
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,14 +9,28 @@ import uuid
 import requests
 import time
 import json
+import typing as t
 
 class scraper:
-    def __init__(self,URL):
+
+    '''
+    This class acts as a web scraper for speedrun.com.
+
+    The scraper is able to search for a game and collect the leaderboard data for every main and miscellaneous category, saving it in a json format.
+
+    Attributes:
+        URL (str): the initial URL for the webdriver, defaults to https://www.speedrun.com 
+    '''
+
+    def __init__(self,URL: str = 'https://www.speedrun.com'):
         self.URL = URL
         self.driver = webdriver.Firefox()
+        self._load_site()
 
-    # Loads site and accepts cookies (on sr.com)
-    def load_site(self) -> webdriver.Firefox:
+    def _load_site(self) -> webdriver.Firefox:
+        '''
+        Loads site and accepts cookies (on sr.com)
+        '''
         self.driver.get(self.URL)
         try:
             accept_cookies_button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]')))
@@ -25,12 +40,17 @@ class scraper:
         except TimeoutException:
             print('[ERROR] timeout exception when trying to accept cookies')
 
-    # Searches for 'query' in the search bar and chooses the first result
-    '''
-    TO DO: -FIGURE OUT HOW TO SEARCH ONLY THE GAMES LIST, IGNORING THE SERIES LIST
-           -MAYBE IMPLEMENT searchGame AND searchUser AS SEPARATE FUNCTIONS
-    '''
-    def search(self,query):
+    
+    # TO DO: -FIGURE OUT HOW TO SEARCH ONLY THE GAMES LIST, IGNORING THE SERIES LIST
+    #        -MAYBE IMPLEMENT searchGame AND searchUser AS SEPARATE FUNCTIONS
+    
+    def search(self,query: str):
+        '''
+        Searches for 'query' in the search bar and chooses the first result
+
+        Args:
+            query (str): the query to be searched
+        '''
         search_bar = self.driver.find_element(by=By.XPATH, value='//*[@type="search"]')
         search_bar.click()
         time.sleep(0.5)
@@ -44,9 +64,13 @@ class scraper:
             print('[ERROR] no results found')
         
 
-    # Collects all the times on the leaderboard for selected category (i.e. PBs of all players with runs on the board) + name of runner + link to VOD
-    # and stores info in a dictionary
     def get_all_cat_PBs(self):
+        '''
+        Collects all the times on the leaderboard for selected category (i.e. PBs of all players with runs on the board) + name of runner + link to VOD
+
+        Returns:
+            dict: a dictionary with info for all PBs for selected category
+        '''
         all_runs = self.driver.find_element(by=By.XPATH, value='//table[@id="primary-leaderboard"]')
         names = all_runs.find_elements(by=By.XPATH, value='//span[@class="nobr" or @class="username"]')
         times = all_runs.find_elements(by=By.XPATH, value='//tr/td[@class="nobr center hidden-xs"][1]')
@@ -61,17 +85,20 @@ class scraper:
             cat_dict['runs'].append({'time': times[i].text, 'name': names[i].text, 'vod': vod_link})
         return cat_dict
 
-    # Switches page to next category. If on last category, returns to the initial category
-    def next_cat(self):
+
+    def next_cat(self) -> t.Optional[bool]:
+        '''
+        Switches page to next category. If on last category, returns to the initial category.
+
+        Returns:
+            bool: True if returning to initial category.
+        '''
         done = False
         try:
             misc_button = self.driver.find_element(by=By.XPATH, value='//a[@class="category category-tab active"]/following-sibling::a[1][@id="miscellaneous"]')
             misc_button.click()
             next_category = self.driver.find_element(by=By.XPATH, value='//a[@class="dropdown-item category"]')
-        except:
-            try:
-                next_category = self.driver.find_element(by=By.XPATH, value='//a[@class="category category-tab active"]/following-sibling::a')
-            except:
+        except:    # myScraper.load_site()
                 try:
                     misc_button = self.driver.find_element(by=By.XPATH, value='//a[@id="miscellaneous"]')
                     misc_button.click()
@@ -83,8 +110,14 @@ class scraper:
         time.sleep(0.5)
         return done # Use this to break a loop cycling through all categories
 
-    # Gets PBs for all categories for game with links and IDs for each category, stores this info in a dictionary
-    def get_all_game_PBs(self):
+
+    def get_all_game_PBs(self) -> dict:
+        '''
+        Gets PBs for all categories for game with links and IDs for each category, stores this info in a dictionary.
+
+        Returns:
+            dict: a dictionary with info for all PBs for selected game
+        '''
         game_dict = {'category': []}
         done = False
         while done == False:
@@ -96,8 +129,7 @@ class scraper:
 
 
 if __name__ == "__main__":
-    myScraper = scraper('https://www.speedrun.com')
-    myScraper.load_site()
+    myScraper = scraper()
     myScraper.search('Spyro the dragon')
     game_id = myScraper.driver.current_url[25:]
     my_dict = myScraper.get_all_game_PBs()

59d2d7ed826ead17b6801add46b6b90b5af1987d
Joe Gocher <joegocher@gmail.com>
2022-06-21 13:24:19 +0100

    -improves restarting with cronjobs

--- srcScraper/file_utils.py
+++ srcScraper/file_utils.py
@@ -16,10 +16,8 @@ DATABASE = 'postgres'
 engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
 engine.connect()
 
-key = input('Access key ID please: ')
-skey = input('Secret key please: ')
 region = 'eu-west-2'
-s3_client = boto3.client('s3',aws_access_key_id=key,aws_secret_access_key=skey,region_name=region)
+s3_client = boto3.client('s3',region_name=region)
 s3 = boto3.resource('s3')
 bucket = 'srdotcom-board-data'
 my_bucket = s3.Bucket(bucket)
